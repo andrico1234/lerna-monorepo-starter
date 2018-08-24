5243530619808e5dcf757d54b4b86abd5b474bba
Andrico <andrico1234@yahoo.co.uk>
2018-08-24 08:32:39 +0100

    fix issue with running the build

--- src/pages/blogItem.js
+++ src/pages/blogItem.js
@@ -6,12 +6,17 @@ const StyledAnchor = styled.a`
   display: block;
 `
 
-export default ({ pathContext: { data }}) => (
-  <div>
-    <h1>{data.title}</h1>
-    <p>{data.content}</p>
-    <StyledAnchor href={data.href}>Full article</StyledAnchor>
-    <Link to="/blog/">Back to Blog</Link>
-  </div>
-)
-
+export default ({ pathContext: { data }}) => {
+  if (!data) {
+    return null
+  }
+  
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.content}</p>
+      <StyledAnchor href={data.href}>Full article</StyledAnchor>
+      <Link to="/blog/">Back to Blog</Link>
+    </div>
+  )
+}
